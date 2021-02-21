30311c260365c10fcdf24332cf002dfaf3155e86
0736b <thanawat_bumpengpun@elearning.cmu.ac.th>
2021-02-21 22:00:42 +0700

    rc4 update

--- rc4.cpp
+++ rc4.cpp
@@ -1,149 +1,116 @@
 #include<iostream>
 #include<vector>
-#include<conio.h>
+#include <algorithm>
 #include<string>
 #include<iomanip>
+#include <windows.h>
 
 using namespace std;
 
-void menuRC4(){
-    cout << ": The RC4 Cipher :" << endl;
-    cout << "------------------" << endl;
-    cout << "[01] History of RC4 Encryption" << endl;
-    cout << "[02] Working of RC4" << endl;
-    cout << "[03] Encryption" << endl;
-    cout << "[04] Decryption" << endl << endl;
-    cout << "Enter number like" << " 01 " << "to select: ";
-}
-void historyrc4(){
-    system("cls");
-    cout << ": History of RC4 Encryption :" << endl << endl;
-    cout << "RC4 means Rivest Cipher 4 invented by Ron Rivest in 1987 for RSA Security.\n It is a Stream Ciphers. Stream Ciphers operate on a stream of data byte by byte.\n RC4 stream cipher is one of the most widely used stream ciphers because of its simplicity and speed of operation.\n It is a variable key-size stream cipher with byte-oriented operations.\n It uses either 64 bit or 128-bit key sizes.\n It is generally used in applications such as Secure Socket Layer (SSL), Transport Layer Security (TSL), and also used in IEEE 802.11 wireless LAN std. ";
-}
-void workingOfRC4(){
-    system("cls");
-    cout << ": Working of RC4 :" << endl << endl;
-    cout << "- Encryption Procedure -" << endl << endl;
-    cout << "1. The user inputs a plain text file and a secret key." << endl;
-    cout << "2. The encryption engine then generates the keystream by using KSA and PRGA Algorithm." << endl;
-    cout << "3. This keystream is now XOR with the plain text, this XORing is done byte by byte to produce the encrypted text." << endl;
-    cout << "4. The encrypted text is then sent to the intended receiver, the \n    intended receiver will then decrypted the text and after decryption, the receiver will get the original plain text." << endl;
-    cout << "- Decryption Procedure -" << endl << endl;
-    cout << "Decryption is achieved by doing the same byte-wise X-OR operation on the Ciphertext.";
-}
-
+// Global Variables
+vector<char> S;
+string key = "";
+string plaintext;
 
 // Array Initialization
-void arrayInit(vector<char> &s){
-    cout << "The first step is the array initialization. It is a character array of size 256" << endl;
-    for(int i = 0; i<256; i++){
+void arrayInit(vector<char> &S){
+    unsigned int i = 0;
         system("cls");
-        s[i] = i;
-        cout << "for every element of the array, we initialize S[i] to i." << endl;
-        cout << "Array initialize.." << endl << endl;
-        cout << "S[" << i << "]" << " = " << i; << endl;
-    }
-    cout << "After this, we will run the KSA algorithm";
-}
-
-// Key Scheduling Algorithm (KSA)
-void keySchedulingAlgo(vector<char> &s, char KSAkey[]){
-    unsigned int i = 0, j = 0;
-    unsigned KSAkeylength = strlen(KSAkey);
-    char tempS;
-
-    for(i = 0; i<256; i++){
-        j = (j + s[i] + KSAkey[ i % KSAkeylength]) % 256;
-        tempS = s[i]; // Swap the S[i] , S[j]
-        s[i] = s[j];
-        s[j] = tempS; 
-    }
-}
-
-void psudoRanGenAlgor(vector<char> &s,)
-
-// RC4 Encryption Processing ..
-void rc4Encrypt(vector<char> &s,string &plaintext){
-    string encrypted;
-    vector<char> KSAkey;
-    char KSAtemp[100];
-    arrayInit(s);
-    cout << "Enter a secret key for KSA algorithm :";
-    cin >> KSAtemp;
-    keySchedulingAlgo(s,KSAtemp);
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-
-}
-
-void commandsCheck(string key,vector<char> &s,){
-    if(key == "01"){
-        historyrc4();
-    }
-    else if(key == "02"){
-        workingOfRC4();
-    }
-    else if(key == "03"){
-        cout << "Enter text for encrypt: ";
-    
-        cout << "Let see how RC4 encryption" << endl;
-        rc4Encrypt(s,plaintext);
-    }
-    else if(key == "04"){
-        cout << "Let see how RC4 decryption" << endl;
-
-    }
-    else if(key == "exit"){
+        cout << ": Array Initialization :" << endl << endl;
+        Sleep(1000);
+        cout << "The first step is the array initialization. It is a character array of size 256" << endl;
+        Sleep(1000);
+        cout << "for every element of the array, we initialize S[i] to i." << endl << endl;
+        Sleep(1000);
+        cout << "[+] Array initialize.." << endl << endl;
+        Sleep(1000);
+    for(i = 0; i<256 ; i++){
+        S.insert(S.begin()+i,i);
+        if((i % 15) == 0){
         system("cls");
-        menuRC4();
-        do{
-        cin >> key;
-        if(key!= "01" || key!= "02"  || key!= "03" || key!= "04"){
-        cout << "Invalid Please Re-enter number 01-04" << endl;
-        system("cls");
-        menuRC4();
+        cout << ": Array Initialization :" << endl << endl;
+        cout << "The first step is the array initialization. It is a character array of size 256" << endl;
+        cout << "for every element of the array, we initialize S[i] to i." << endl << endl;
+        cout << "[+] Array initialize.." << endl << endl;
+        cout << "         " << "S[" << i << "]" << " = " << i << endl << endl;
         }
-        else{
-         break;
-         }
-        }while(true);
+        Sleep(10);
     }
+    Sleep(1000);
+    cout << "the array will look like S[] = {0, 1, 2, 3, ------, 254, 255}" << endl;
+    Sleep(1000);
+    cout << "After this, we will run the KSA algorithm.";
 }
 
+// Key Scheduling Algorithm
+void keySchedulAlgor(string &key,vector<char> &S){
+    Sleep(1000);
+    cout << ": Key Scheduling Algorithm :" << endl << endl;
+    unsigned int i = 0, j = 0;
+    Sleep(1000);
+    cout << "[+] Your Plain Text: " << plaintext << endl << endl;
+    Sleep(500);
+    cout << "[+] Enter the secret key: ";
+    cin >> key;
+    Sleep(500);
+    cout << "    Your Secret Key: " << key << endl;
+    for (i = 0; i < 256; i++)
+    {
+        j = (j + S[i] + key.at(i % key.length())) % 256;
+        swap(S[i], S[j]);
+    }
+}
 
 int main(){
-    string key;
-    string plaintext;
-    vector<char> s;
+    string select;
     system("cls");
-    menuRC4();
-
-    do{
-    cin >> key;
-    if(key!= "01" || key!= "02"  || key!= "03" || key!= "04"){
-        cout << "Invalid Please Re-enter number 01-04" << endl;
+    cout << "The RC4 cipher" << endl << endl;
+    cout << "[00] How it work ?" << endl;
+    cout << "[01] Encryption" << endl;
+    cout << "[02] Decryption" << endl;
+    cout << "Select by enter number 00-02 : ";
+    cin >> select;
+    if(select == "01"){
         system("cls");
-        menuRC4();
+        Sleep(500);
+        cout << "[ Encryption ]";
+        cout << endl << endl << "Input the text: ";
+        cin >> plaintext;
+        cout << "[+] Your Plain Text: " << plaintext << endl << endl;
+        Sleep(500);
+        cout << "Let's encrypt your text.." << endl;
+        cout << "Press anykey to next step.";
+        cin.ignore();
+        cin.get();
+        Sleep(2000);
+        arrayInit(S);
+        Sleep(500);
+        cout << endl << endl;
+        Sleep(500);
+        cout << "___________________________________________________________________________________" << endl << endl;
+        Sleep(1000);
+        cout << "|\n";
+        Sleep(500);
+        cout << "|\n";
+        Sleep(500);
+        cout << "|\n";
+        Sleep(500);
+        cout << "|\n";
+        Sleep(500);
+        cout << "|\n";
+        Sleep(500);
+        cout << "V";
+        cout << endl << endl;
+        Sleep(1000);
+        cout << ": Key Scheduling Algorithm :" << endl << endl;
+        Sleep(500);
+        cout << "Press anykey to next step.";
+        cin.ignore();
+        cin.get();
+        Sleep(1000);
+        system("cls");
+        keySchedulAlgor(key,S);
     }
-    else{
-        break;
-    }
-    }while(true);
 
-    commandsCheck(key,s);
-
-    
-
-    
-    
-    
     
 }
