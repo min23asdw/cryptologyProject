614194f3dbdc7ddb36c4ac8f1840d187c5a56ce8
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-03-09 01:23:44 +0700

    Read byte, white test img, delete small color img

--- stegPPM.cpp
+++ stegPPM.cpp
@@ -15,15 +15,23 @@ public:
     int color = 0;
     int row = 0;
     int col = 0;
+    struct Rgb
+    {
+        unsigned char r, g, b;
+    };
+
+    Rgb *pixels = nullptr;
+
     string version = "";
 
     void openFile(string filename);
     void readHeader();
+    void readImageData();
 };
 
 void stegPPM::openFile(string filename)
 {
-    inFile.open(filename);
+    inFile.open(filename, ios::binary);
 }
 
 void stegPPM::readHeader()
@@ -65,10 +73,28 @@ void stegPPM::readHeader()
     }
 }
 
+void stegPPM::readImageData()
+{
+    inFile.ignore(256, '\n');
+    pixels = new Rgb[width*height];
+    unsigned char pix[3];
+    for(int i = 0; i < width * height; i++)
+    {
+        inFile.read(reinterpret_cast<char *> (pix), 3);
+        pixels[i].r = pix[0];
+        pixels[i].g = pix[1];
+        pixels[i].b = pix[2];
+    }
+
+    cout << static_cast<int>(pixels[1].r);
+
+    delete [] pixels;
+}
+
 int main()
 {
     stegPPM steg;
-    steg.openFile("testImg/test.ppm");
+    steg.openFile("testImg/white.ppm");
 
     try
     {
@@ -79,4 +105,6 @@ int main()
         std::cerr << e.what() << '\n';
         cout << "Corrupted file, or unsupported file type " << '\n';
     }
+
+    steg.readImageData();
 }
